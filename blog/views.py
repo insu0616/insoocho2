12cbd45f7d1d8e3bff1705fa58f3c9d0fff0a345
insoo <inu0616@gmail.com>
2016-05-25 20:41:07 +0900

    detail 구현

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from .models import Post, Comment
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', {'post_list': post_list})
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})
