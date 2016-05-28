fb4a843b213e2c876d8ffca45d89468b04bff4e0
insoo <inu0616@gmail.com>
2016-05-28 16:03:35 +0900

    프로젝트/settings.common.py

--- urls.py
+++ urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
@@ -26,7 +26,8 @@ def hello(request):
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$' , hello),
+    url(r'^blog/' , include('blog.urls')),
+    url(r'^board/' , include('board.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
